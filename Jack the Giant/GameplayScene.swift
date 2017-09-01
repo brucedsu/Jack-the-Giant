e2243472bb0b504f303967c39f6553632e9ee2b1
brucedsu <deisu.tech@gmail.com>
2017-09-01 09:26:10 -0700

    Added main camera

--- Jack the Giant/GameplayScene.swift
+++ Jack the Giant/GameplayScene.swift
@@ -10,6 +10,8 @@ import SpriteKit
 
 class GameplayScene: SKScene {
     
+    var mainCamera: SKCameraNode?
+    
     var player: Player?
     
     var canMove = false
@@ -18,14 +20,11 @@ class GameplayScene: SKScene {
     var center: CGFloat?
     
     override func didMove(to view: SKView) {
-        center = self.scene!.size.width / self.scene!.size.height
-        
-        // print("width: \(self.scene!.size.width), height: \(self.scene!.size.height)")
-        
-        player = self.childNode(withName: "Player") as? Player
+        initializeVariables()
     }
     
     override func update(_ currentTime: TimeInterval) {
+        moveCamera()
         managePlayer()
     }
     
@@ -35,10 +34,12 @@ class GameplayScene: SKScene {
             
             // print("x: \(location.x), y: \(location.y)")
             
-            if location.x > center! {
+            if location.x > 0 {
                 moveLeft = false
+                player?.animatePlayer(moveLeft: moveLeft)
             } else {
                 moveLeft = true
+                player?.animatePlayer(moveLeft: moveLeft)
             }
         }
         
@@ -47,12 +48,26 @@ class GameplayScene: SKScene {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         canMove = false
+        player?.stopPlayerAnimation()
+    }
+    
+    func initializeVariables() {
+        center = self.scene!.size.width / self.scene!.size.height
+        
+        player = self.childNode(withName: "Player") as? Player
+        player?.initializePlayerAndAnimations()
+        
+        mainCamera = self.childNode(withName: "Main Camera") as? SKCameraNode
     }
     
     func managePlayer() {
         if canMove {
             player?.movePlayer(moveLeft: moveLeft)
         }
+    }
+    
+    func moveCamera() {
+        mainCamera?.position.y -= 3
     }
     
 }
