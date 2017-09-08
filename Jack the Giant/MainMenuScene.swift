4455135fcbf061db77c596e668ac3abc9f5e6d4f
brucedsu <deisu.tech@gmail.com>
2017-09-08 08:56:23 -0700

    Create MainMenuScene.swift

---
+++ Jack the Giant/MainMenuScene.swift
@@ -0,0 +1,17 @@
+//
+//  MainMenuScene.swift
+//  Jack the Giant
+//
+//  Created by Hejia Su on 9/8/17.
+//  Copyright © 2017 Watermelon Studio. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenuScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
