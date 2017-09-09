1c5ee3b7aad0b0faa6a5bfb4f40f1fb8c00ea2d5
brucedsu <deisu.tech@gmail.com>
2017-09-08 17:27:55 -0700

    Added more navigations

--- Jack the Giant/MainMenuScene.swift
+++ Jack the Giant/MainMenuScene.swift
@@ -10,22 +10,38 @@ import SpriteKit
 
 class MainMenuScene: SKScene {
     
+    var startGameButton: SKSpriteNode?
     var highScoreButton: SKSpriteNode?
+    var optionButton: SKSpriteNode?
     
     override func didMove(to view: SKView) {
+        startGameButton = self.childNode(withName: "Start Game") as? SKSpriteNode
         highScoreButton = self.childNode(withName: "High Score") as? SKSpriteNode
+        optionButton = self.childNode(withName: "Options") as? SKSpriteNode
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
             
-            if self.nodes(at: location).contains(highScoreButton!) {  
+            if self.nodes(at: location).contains(startGameButton!) {
+                let scene = GameplayScene(fileNamed: "GameplayScene")
+                scene!.scaleMode = .aspectFill
+                self.view?.presentScene(
+                    scene!,
+                    transition: SKTransition.crossFade(withDuration: 1))
+            } else if self.nodes(at: location).contains(highScoreButton!) {
                 let scene = HighScoreScene(fileNamed: "HighScoreScene")
                 scene!.scaleMode = .aspectFill
                 self.view?.presentScene(
                     scene!,
                     transition: SKTransition.doorsOpenVertical(withDuration: 1))
+            } else if self.nodes(at: location).contains(optionButton!) {
+                let scene = OptionScene(fileNamed: "OptionScene")
+                scene!.scaleMode = .aspectFill
+                self.view?.presentScene(
+                    scene!,
+                    transition: SKTransition.flipHorizontal(withDuration: 1))
             }
         }
     }
--- Jack the Giant/OptionScene.swift
+++ Jack the Giant/OptionScene.swift
@@ -10,8 +10,24 @@ import SpriteKit
 
 class OptionScene: SKScene {
     
+    var backButton: SKSpriteNode?
+    
     override func didMove(to view: SKView) {
-        
+        backButton = self.childNode(withName: "Back Button") as? SKSpriteNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            if nodes(at: location).contains(backButton!) {
+                let scene = MainMenuScene(fileNamed: "MainMenuScene")
+                scene!.scaleMode = .aspectFill
+                self.view?.presentScene(
+                    scene!,
+                    transition: SKTransition.flipHorizontal(withDuration: 1))
+            }
+        }
     }
     
 }
