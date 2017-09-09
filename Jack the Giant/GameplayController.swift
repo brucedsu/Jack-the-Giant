23016475460dbd3338c2516d42869e1d4e8ff361
brucedsu <deisu.tech@gmail.com>
2017-09-08 21:42:09 -0700

    Created GameplayController

---
+++ Jack the Giant/GameplayController.swift
@@ -0,0 +1,20 @@
+//
+//  GameplayController.swift
+//  Jack the Giant
+//
+//  Created by Hejia Su on 9/8/17.
+//  Copyright © 2017 Watermelon Studio. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameplayController {
+
+    static let instance = GameplayController()
+    
+    private init() {
+        
+    }
+    
+}
